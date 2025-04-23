349f73712a6c5dbd36e9ea751dac98e6d457449d
alba <alba2006gs@gmail.com>
2025-04-23 12:50:34 +0200

    first restaB in main

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import utilitiesA as utilA
+import utilitiesB as utilB
 
 def main():
     a = 7
     b = 5 
     c = utilA.sumaA(a, b)
+
     print(f"el valos de la suma de a y b es :{c}")
+
+    d = utilB.restaB(a, b)
+    print(f"el valor de la resta de a y b es :{d}")
+
+
 
 if __name__ == "__main__":
     main()
