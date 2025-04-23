a43982566bbbbecc03ab583498b47cb5913348e1
alba <alba2006gs@gmail.com>
2025-04-23 12:56:32 +0200

    saludamos desde A

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import utilitiesA as utilA
 def main():
     a = 7
     b = 5 
+
     c = utilA.sumaA(a, b)
-    print(f"el valos de la suma de a y b es :{c}")
+
+    print(f"el valor de la suma de a y b es :{c}")
+
+    print("hola mundo desde A")
+    print("hola mundo somos del equipo AB")
 
 if __name__ == "__main__":
     main()
